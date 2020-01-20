298673011aa0201e40c950b5e42940b376f8b377
Эдуард <evoronin2002@yandex.ru>
2020-01-20 20:35:58 +0300

    commit load_image

--- main.py
+++ main.py
@@ -35,3 +35,18 @@ back_sprites = pygame.sprite.Group()
 pause_sprites = pygame.sprite.Group()
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
+bots = pygame.sprite.Group()
+life = pygame.sprite.Group()
+cherries = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
